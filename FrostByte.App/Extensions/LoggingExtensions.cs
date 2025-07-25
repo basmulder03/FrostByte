44b35f5adde2992d129c0a0eaebc58abc56a4253
Bas Mulder <bas.d.mulder@gmail.com>
2025-07-25 23:01:06 +0200

    Improves application logging
    Configures Serilog for enhanced logging, including console output and a standardized log template.
    
    Adds console sink for real-time monitoring and ensures consistent formatting across debug, console, and file outputs.

--- FrostByte.App/Extensions/LoggingExtensions.cs
+++ FrostByte.App/Extensions/LoggingExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
 using Serilog;
+using Serilog.Events;
 
 namespace FrostByte.App.Extensions;
 
@@ -9,15 +10,23 @@ public static class LoggingExtensions
     {
         var logFileLocation = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "FrostByte", "Logs");
         Directory.CreateDirectory(logFileLocation);
+        const string logTemplate = "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}";
 
         Log.Logger = new LoggerConfiguration()
             .MinimumLevel.Debug()
             .Enrich.FromLogContext()
-            .WriteTo.Debug()
+            .WriteTo.Debug(
+                outputTemplate: logTemplate
+            )
+            .WriteTo.Console(
+                restrictedToMinimumLevel: LogEventLevel.Information,
+                outputTemplate: logTemplate
+            )
             .WriteTo.File(
                 path: logFileLocation,
                 rollingInterval: RollingInterval.Day,
-                retainedFileCountLimit: 7
+                retainedFileCountLimit: 7,
+                outputTemplate: logTemplate
             )
             .CreateLogger();
 
