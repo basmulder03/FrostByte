b130b7b932f9f46da356f5e5aa4e68c540f073db
Bas Mulder <bas.d.mulder@gmail.com>
2025-08-09 18:19:10 +0200

    Adds authentication handling to app shell
    Improves the application's startup by ensuring authentication is checked asynchronously, preventing potential UI blocking.
    
    Adds exception handling during navigation to the authentication page and logs any errors encountered, providing better error diagnostics.

--- FrostByte.App/AppShell.cs
+++ FrostByte.App/AppShell.cs
@@ -1,18 +1,21 @@
 ﻿using FrostByte.Presentation.ViewModels;
 using FrostByte.Presentation.Views;
+using Microsoft.Extensions.Logging;
 
 namespace FrostByte.App;
 
 public partial class AppShell : Shell
 {
     private readonly Func<Page> _authPageFactory;
+    private readonly ILogger<AppShell> _logger;
     private readonly AppShellVm _vm;
 
     public AppShell(Func<CalendarPage> calendarPageFactory, Func<DayPage> dayPageFactory,
-        Func<AuthPage> authPageFactory, AppShellVm vm)
+        Func<AuthPage> authPageFactory, AppShellVm vm, ILogger<AppShell> logger)
     {
         _vm = vm ?? throw new ArgumentNullException(nameof(vm));
         _authPageFactory = authPageFactory ?? throw new ArgumentNullException(nameof(authPageFactory));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
 
         FlyoutIsPresented = false;
         FlyoutBehavior = FlyoutBehavior.Disabled;
@@ -21,7 +24,7 @@ public partial class AppShell : Shell
 
         // Subscribe to the authentication required event
         _vm.AuthenticationRequired += OnAuthenticationRequired;
-        _ = _vm.CheckAuthenticationAsync();
+        Task.Run(async () => await _vm.CheckAuthenticationAsync());
     }
 
     private void AddPageToShell(Func<Page> pageFactory, string route, bool navBarVisible)
@@ -38,6 +41,13 @@ public partial class AppShell : Shell
 
     private async void OnAuthenticationRequired(object? sender, EventArgs e)
     {
-        await Navigation.PushModalAsync(_authPageFactory());
+        try
+        {
+            await Navigation.PushModalAsync(_authPageFactory());
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Failed to navigate to authentication page.");
+        }
     }
 }
